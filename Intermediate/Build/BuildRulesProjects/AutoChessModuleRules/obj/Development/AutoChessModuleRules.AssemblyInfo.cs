0de79dd2e9445fc3f5aa99ee5ace7153cbc2a971
DumboLZ <zyl.lau@foxmail.com>
2025-11-27 17:50:48 +0800

    上传dll

--- Intermediate/Build/BuildRulesProjects/AutoChessModuleRules/obj/Development/AutoChessModuleRules.AssemblyInfo.cs
+++ Intermediate/Build/BuildRulesProjects/AutoChessModuleRules/obj/Development/AutoChessModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoChessModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb95b593fe527695e892454e5d842489641984bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f19de69036d6aabbe2f7f0603f6efae19cac5a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoChessModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoChessModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
